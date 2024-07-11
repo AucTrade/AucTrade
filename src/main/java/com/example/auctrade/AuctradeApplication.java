a26e84223f5ffbde492d883d8b4cbac6f90621d4
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-11 18:03:49 +0900

    initial setting

---
+++ src/main/java/com/example/auctrade/AuctradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctradeApplication.class, args);
+    }
+
+}
