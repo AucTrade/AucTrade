3d888ae1c525f70d7e7278ff132c2758dfb31fe7
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-16 23:45:42 +0900

    feat: JPA 기반 경매 엔티티 생성

--- src/main/java/com/example/auctrade/domain/auction/entity/Auction.java
+++ src/main/java/com/example/auctrade/domain/auction/entity/Auction.java
@@ -1,26 +1,37 @@
 package com.example.auctrade.domain.auction.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "auctions")
+@EntityListeners(AuditingEntityListener.class)
 public class Auction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Long id; // 경매장 id
 
-    @Column(name = "price", nullable = false)
-    private int price; // 계속 업데이트하다가 경매 종료됐을 때의 price 를 최종낙찰가로
+    @Column(name = "title", nullable = false)
+    private String title; // 경매 제목
+
+    @Column(name = "introduce")
+    private String introduce; // 경매 설명
+
+    @Column(name = "personnel", nullable = false)
+    private int personnel; // 경매 참여인(최대)
 
     @Column(name = "products", nullable = false)
     private String products; // Product 엔티티 생성시 수정 필요
@@ -28,27 +39,23 @@ public class Auction {
     @Column(name = "user_id", nullable = false)
     private Long userId; // User 엔티티 생성시 수정 필요
 
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate; // 경매 시작예고시간, 시간 포맷팅 고려 필요
+
     @CreatedDate
     @Column(name = "created", updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime createdAt; // 경매 게시일자
-
-    @Column(name = "auction_date", nullable = false)
-    private LocalDateTime auctionDate; // 경매 시작예고시간, 시간 포맷팅 고려 필요
+    private LocalDateTime createdAt; // 경매 채팅방 생성일자
 
     @Column(name = "minimum_price", nullable = false)
-    private int minimumPrice;
+    private int minimumPrice; // 최소입찰금액
 
-    @Column(name = "end", nullable = false)
-    private boolean end; // price 가 진행 가격인지 최종 낙찰 가격인지
+    @Column(name = "price", nullable = false)
+    private int price; // 계속 업데이트하다가 경매 종료됐을 때의 price 를 최종낙찰가로
 
-    @Builder
-    public Auction(int price, String products, Long userId, LocalDateTime auctionDate, int minimumPrice) {
-        this.price = price;
-        this.products = products;
-        this.userId = userId;
-        this.auctionDate = auctionDate;
-        this.minimumPrice = minimumPrice;
-        this.end = false;
-    }
+    @Column(name = "ended", nullable = false)
+    private boolean ended; // price 가 진행 가격인지 최종 낙찰 가격인지
+
+    @Column(name = "finish_date", nullable = false)
+    private LocalDateTime finishDate; // 경매 종료예고시간, 시간 포맷팅 고려 필요
 }
