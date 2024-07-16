489b69db4795ae8f1385dbc902ef22e9fd2bbf98
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-17 00:48:41 +0900

    feat: 상품 관련 엔티티 클래스 정의

--- src/main/java/com/example/auctrade/domain/product/entity/Product.java
+++ src/main/java/com/example/auctrade/domain/product/entity/Product.java
@@ -1,4 +1,27 @@
 package com.example.auctrade.domain.product.entity;
 
+import com.example.auctrade.domain.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "detail")
+    private String detail;
+
+    @ManyToOne
+    @JoinColumn(name = "category", nullable = false)
+    private ProductCategory category;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 상품 등록자
 }
---
+++ src/main/java/com/example/auctrade/domain/product/entity/ProductCategory.java
@@ -0,0 +1,15 @@
+package com.example.auctrade.domain.product.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+}
