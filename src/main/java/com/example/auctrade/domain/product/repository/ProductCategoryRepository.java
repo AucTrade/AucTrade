65c650901a15755d1fe9d601fecd792979c9378f
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-19 19:01:01 +0900

    feat: 상품 카테고리 dao 구축

---
+++ src/main/java/com/example/auctrade/domain/product/repository/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.auctrade.domain.product.repository;
+
+import com.example.auctrade.domain.product.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+}
