fa56831707c79afa0b0d750b1de0967c2bf34fa5
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-20 20:44:13 +0900

    test: 웹소켓 연결 테스트 확인, 성공

--- src/test/java/com/example/auctrade/chat/WebSocketTest.java
+++ src/test/java/com/example/auctrade/chat/WebSocketTest.java
@@ -1,23 +1,31 @@
 package com.example.auctrade.chat;
 
+import com.example.auctrade.domain.chat.dto.MessageDTO;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // 서버 포트번호 랜덤 설정, @LocalServerPort 기반 할당
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebSocketTest {
 
+    // 로그 기록
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @LocalServerPort
@@ -45,4 +53,16 @@ class WebSocketTest {
         this.url = String.format("ws://localhost:%d/stomp/chat", port);
     }
 
+    // 웹소켓 연결 확인
+    @Test
+    void testWebSocketConnection() throws Exception {
+        // given & when, then
+        WebSocketStompClient stompClient = getStompClient();
+        StompSession session = stompClient.connectAsync(
+                url, new StompSessionHandlerAdapter() {}).get(5, TimeUnit.SECONDS);
+
+        log.info("세션 연결: {}", session.isConnected());
+
+        assertTrue(session.isConnected(), "WebSocket 연결 성공");
+    }
 }
