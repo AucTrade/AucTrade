202b0bcd2dbace582780245ff2678830b707f33f
burning23185 <burning23@naver.com>
2024-07-21 17:03:28 +0900

    fix: error code 수정

--- src/main/java/com/example/auctrade/global/exception/ErrorCode.java
+++ src/main/java/com/example/auctrade/global/exception/ErrorCode.java
@@ -27,8 +27,6 @@ public enum ErrorCode {
     POST_GET_NOT_PERMISSION(FORBIDDEN, "해당 게시물을 조회할 권한이 없습니다."),
     POST_UPDATE_NOT_PERMISSION(FORBIDDEN, "해당 게시물을 수정할 권한이 없습니다."),
     POST_DELETE_NOT_PERMISSION(FORBIDDEN, "해당 게시물을 삭제할 권한이 없습니다."),
-    POST_COMMENT_DELETE_NOT_PERMISSION(FORBIDDEN, "해당 댓글을 삭제할 권한이 없습니다."),
-    PLAN_VOTE_IS_CLOSED(FORBIDDEN, "해당 투표장은 기간만료로 닫혀 투표 권한이 없습니다."),
     POST_ACCESS_NOT_PERMISSION(FORBIDDEN, "접근할 수 없는 게시물입니다."),
 
     // 404 NOT_FOUND: 잘못된 리소스 접근
@@ -38,22 +36,11 @@ public enum ErrorCode {
     PRODUCT_NOT_FOUND(NOT_FOUND, "해당 물품을 찾을 수 없습니다."),
     PRODUCT_CATEGORY_NOT_FOUND(NOT_FOUND, "해당 물품 카테고리를 찾을 수 없습니다."),
     POST_IMAGE_NOT_FOUND(NOT_FOUND, "해당 게시글의 이미지를 찾을 수 없습니다."),
-    POST_SCRAP_NOT_FOUND(NOT_FOUND, "해당 게시글은 스크랩되어 있지 않습니다."),
-    POST_LIKE_NOT_FOUND(NOT_FOUND, "해당 게시글은 좋아요가 되어 있지 않습니다."),
-    POST_COMMENT_NOT_FOUND(NOT_FOUND, "해당 댓글을 찾을 수 없습니다."),
-    PLAN_NOT_FOUND(NOT_FOUND, "해당 여행 플랜을 찾을 수 없습니다."),
-    DAY_PLAN_NOT_FOUND(NOT_FOUND, "해당 일일 플랜을 찾을 수 없습니다."),
-    UNIT_PLAN_NOT_FOUND(NOT_FOUND, "해당 단위 플랜을 찾을 수 없습니다."),
-    PLAN_COMMENT_NOT_FOUND(NOT_FOUND, "해당 댓글을 찾을 수 없습니다."),
-    PLAN_VOTE_NOT_FOUND(NOT_FOUND, "해당 투표장을 찾을 수 없습니다."),
-    VOTE_PAPER_NOT_FOUND(NOT_FOUND, "해당 투표지를 찾을 수 없습니다."),
     USER_NOT_FOUND(NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
 
     // 409 CONFLICT: 중복된 리소스 (요청이 현재 서버 상태와 충돌될 때)
     DUPLICATE_EMAIL(CONFLICT, "이미 존재하는 이메일입니다."),
     DUPLICATE_NICKNAME(CONFLICT, "이미 존재하는 닉네임입니다."),
-    DUPLICATE_SCRAP(CONFLICT, "이미 스크랩을 하였습니다."),
-    DUPLICATE_LIKE(CONFLICT, "이미 좋아요를 누르셨습니다."),
 
     // 500 INTERNAL SERVER ERROR
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "내부 서버 에러입니다.");
