f1421af50f6aba90940c87eab0b702addd75e0cc
kimD0ngjun <chickentasty0112@gmail.com>
2024-07-31 01:50:31 +0900

    feat: 인증 객체 기반 메세지 인증 구현

--- src/main/java/com/example/auctrade/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/example/auctrade/domain/chat/controller/ChatMessageController.java
@@ -1,7 +1,5 @@
 package com.example.auctrade.domain.chat.controller;
 
-import com.example.auctrade.domain.auction.dto.AuctionDTO;
-import com.example.auctrade.domain.auction.service.BidLogServiceImpl;
 import com.example.auctrade.domain.chat.dto.MessageDTO;
 import com.example.auctrade.domain.chat.service.ChatMessageServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +7,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 import static com.example.auctrade.global.constant.Constants.CHAT_AUCTION_DESTINATION;
 
@@ -21,7 +21,10 @@ public class ChatMessageController {
     private final ChatMessageServiceImpl chatMessageService;
 
     @MessageMapping(value = "/chat/enter")
-    public void enter(MessageDTO.Create message){
+    public void enter(MessageDTO.Create message, Principal principal){
+        log.info("{} 채팅방 입장", principal.getName());
+        message.setUsername(principal.getName());
+
         MessageDTO.Get responseDto = chatMessageService.saveChatMessage(message);
         if( responseDto != null)
             sendingOperations.convertAndSend(CHAT_AUCTION_DESTINATION + message.getAuctionId(), responseDto);
@@ -29,7 +32,10 @@ public class ChatMessageController {
 
 
     @MessageMapping(value = "/chat/message")
-    public void message(MessageDTO.Create message){
-            sendingOperations.convertAndSend(CHAT_AUCTION_DESTINATION + message.getAuctionId(), message);
+    public void message(MessageDTO.Create message, Principal principal){
+        log.info("{} 메세지 전송: {}", principal.getName(), message.getMessage());
+        message.setUsername(principal.getName());
+
+        sendingOperations.convertAndSend(CHAT_AUCTION_DESTINATION + message.getAuctionId(), message);
     }
 }
