0973b3dc3cfcd548e438d45aad868ed2040e3228
burning23185 <burning23@naver.com>
2024-08-19 18:46:05 +0900

    fix: 경매 입찰 관련 예외 처리 추가

--- src/main/java/com/example/auctrade/domain/user/service/UserService.java
+++ src/main/java/com/example/auctrade/domain/user/service/UserService.java
@@ -1,7 +1,6 @@
 package com.example.auctrade.domain.user.service;
 
 import com.example.auctrade.domain.user.dto.UserDTO;
-import com.example.auctrade.domain.user.entity.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
