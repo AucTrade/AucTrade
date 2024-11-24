685b951d28e833632edf98e8a9ffceb88a72bf0a
burning23185 <burning23@naver.com>
2024-11-24 16:44:42 +0900

    fix: bid 관련 exception 리펙토링

--- src/main/java/com/example/auctrade/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/example/auctrade/global/exception/GlobalControllerAdvice.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Path;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -21,10 +23,18 @@ import java.util.stream.StreamSupport;
 
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalControllerAdvice {
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(DataAccessException ex) {
+        log.error("DataAccessException: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database connection error. Please try again later.");
+    }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest httpServletRequest) {
+        log.error("MethodArgumentNotValidException: {}",e.getMessage());
         List<CustomError> errorList = new ArrayList<>();
         BindingResult bindingResult = e.getBindingResult();
         bindingResult.getAllErrors().forEach(error -> {
@@ -53,6 +63,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResponseEntity constraintViolationException(ConstraintViolationException e, HttpServletRequest httpServletRequest) {
+        log.error("MethodArgumentNotValidException: {}",e.getMessage());
         List<CustomError> errorList = new ArrayList<>();
 
         e.getConstraintViolations().forEach(error -> {
@@ -78,6 +89,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = MissingServletRequestParameterException.class)
     public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e, HttpServletRequest httpServletRequest) {
+        log.error("MissingServletRequestParameterException: {}", e.getMessage());
         List<CustomError> errorList = new ArrayList<>();
 
         errorList.add(CustomError.builder()
@@ -96,6 +108,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = JsonProcessingException.class)
     public ResponseEntity jsonProcessingException(JsonProcessingException e, HttpServletRequest httpServletRequest) {
+        log.error("jsonProcessingException: {}", e.getMessage());
         List<CustomError> errorList = new ArrayList<>();
 
         errorList.add(CustomError.builder()
@@ -113,6 +126,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity customException(CustomException e, HttpServletRequest httpServletRequest) {
+        log.error("CustomException: {}", e.getMessage());
         ErrorCode errorCode = e.getErrorCode();
 
         CustomErrorResponse errorResponse = CustomErrorResponse.builder()
